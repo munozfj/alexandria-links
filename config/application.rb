6c3096da82766a86559068d452516741b0aabd20
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-30 16:30:34 -0300

    Se modifico el archivo config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,10 @@ module AlexandriaLinks
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    ##FJM
+    # Allow compilation of assests on Heroku
+    config.assets.initialize_on_precompile = false
+
   end
 end
