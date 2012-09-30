dbdcc0b2e9330917a51f7afc3651071152346bc0
Fernando J. Muñoz <munozfj@hotmail.com>
2012-09-30 18:01:22 -0300

    Se genero una pagina para mostrar todo el catalogo de libros

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 AlexandriaLinks::Application.routes.draw do
+  #get "store/index"
+
   resources :books
 
   resources :articles
 
-  root :to => 'books#index'
+  root :to => 'store#index', :as => 'store'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
