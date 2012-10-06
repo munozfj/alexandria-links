945a28940d106cd09e7b36df0ff5b67b2c5bf90d
Fernando J. Muñoz <munozfj@hotmail.com>
2012-10-06 17:45:13 -0300

    Traduccion de usuarios

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < ActionController::Base
     def authorize
       if request.format == Mime::HTML 
         unless User.find_by_id(session[:user_id])
-          redirect_to login_url, notice: "Please log in"
+          redirect_to login_url
         end
       else
         authenticate_or_request_with_http_basic do |username, password|
