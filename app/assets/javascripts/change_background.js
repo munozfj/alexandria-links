7402b030c680b44a0e7d2466da0f7f989db1ca9e
Fernando J. Muñoz <munozfj@hotmail.com>
2012-10-09 08:27:17 -0300

    Tramitamiento de background durante la sesion

--- app/assets/javascripts/change_background.js
+++ app/assets/javascripts/change_background.js
@@ -2,19 +2,21 @@ function cambio(valor)
 {
   /*alert("me llego el valor " + valor);*/
 
+  sessionStorage.setItem('tipo_background',valor);
+
   if(valor == "violet")
   {
     document.body.style.color="white";
 	  document.body.style.backgroundImage="url('/assets/background-violet.jpg')";
   }
-  else if(valor == "black")
+  else if(valor == "green")
   {
-    document.body.style.color="white";
-	  document.body.style.backgroundImage="url('/assets/background-black.jpg')";
+    document.body.style.color="yellow";
+	  document.body.style.backgroundImage="url('/assets/background-green.jpg')";
   } 
   else if(valor == "blue")
   {
-    document.body.style.color="orange";
+    document.body.style.color="yellow";
 	  document.body.style.backgroundImage="url('/assets/background-blue.jpg')";
   } 
   else if(valor == "brown")
@@ -30,18 +32,31 @@ function cambio(valor)
   else if(valor == "wood")
   {
     document.body.style.color="black";
-	  document.body.style.backgroundImage="url('/assets/background-wood.png')";
-  } 
-  else if(valor == "verticalwood")
-  {
-    document.body.style.color="black";
 	  document.body.style.backgroundImage="url('/assets/background-wood-vertical.png')";
+  } 
+  else if(valor == "red")
+  {
+    document.body.style.color="gray";
+	  document.body.style.backgroundImage="url('/assets/background-red.jpg')";
   } 
   else 
   {
-    document.body.style.color="black";
-    document.body.style.backgroundImage="url('/assets/background-wood-vertical.png')";
+    document.body.style.color="yellow";
+    document.body.style.backgroundImage="url('/assets/background-green.jpg')";
   }
   
+  /*alert("Me voy con:"+sessionStorage.getItem('tipo_background'));*/
+}
 
+function determinar_cambio()
+{
+  valor=sessionStorage.getItem('tipo_background');
+
+  if (valor == null )
+    valor = document.getElementById("change_background").value;
+
+  document.getElementById("change_background").value = valor;
+
+  /*alert("Value en onload:"+valor);*/
+  cambio(valor);
 }
