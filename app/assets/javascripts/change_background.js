0d5f00b00107e34d1182b273ba8cae8d2076d5c6
Fernando J. Muñoz <munozfj@hotmail.com>
2012-10-11 18:38:57 -0300

    Modificacion de modales, fondos y botones

--- app/assets/javascripts/change_background.js
+++ app/assets/javascripts/change_background.js
@@ -4,41 +4,27 @@ function cambio(valor)
 
   sessionStorage.setItem('tipo_background',valor);
 
-  if(valor == "violet")
-  {
-    document.body.style.color="white";
-	  document.body.style.backgroundImage="url('/assets/background-violet.jpg')";
-  }
-  else if(valor == "blue")
-  {
-    document.body.style.color="white";
-	  document.body.style.backgroundImage="url('/assets/background-blue.jpg')";
-  } 
-  else if(valor == "brown")
-  {
-    document.body.style.color="white";
-	  document.body.style.backgroundImage="url('/assets/background-brown.jpg')";
-  } 
-  else if(valor == "pink")
+  if(valor == "pink")
   {
     document.body.style.color="white";
 	  document.body.style.backgroundImage="url('/assets/background-pink.jpg')";
   } 
-  else if(valor == "wood")
-  {
-    document.body.style.color="black";
-	  document.body.style.backgroundImage="url('/assets/background-wood-vertical.png')";
-  } 
+  
   else if(valor == "red")
   {
     document.body.style.color="white";
 	  document.body.style.backgroundImage="url('/assets/background-red.png')";
   } 
-  else 
+  else if(valor == "green")
   {
     document.body.style.color="orange";
     document.body.style.backgroundImage="url('/assets/background-green.jpg')";
   }
+  else
+  {
+    document.body.style.color="black";
+	  document.body.style.backgroundImage="url('/assets/background-wood-vertical.png')";
+  } 
   
   /*alert("Me voy con:"+sessionStorage.getItem('tipo_background'));*/
 }
